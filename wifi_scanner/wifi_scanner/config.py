27a1a500ba119fc7c56e30087afa71e01e5dd8d4
michaelgy <michael801898@gmail.com>
2020-02-24 10:41:16 -0500

    Create config.py

---
+++ wifi_scanner/wifi_scanner/config.py
@@ -0,0 +1,4 @@
+sample_time = 10#10800 #seconds [3 hours]
+reconnect_time = 3#600 #seconds [10 minutes]
+files_path = "/home/pi/Documents/nm_dbus_python/wifi_scanner"
+datetime_format = "%Y-%m-%d_%H-%M-%S"
