f3c8a5a70d4050764ee8541ce6775c662b0e8334
michaelgy <michael801898@gmail.com>
2020-03-10 21:25:32 -0500

    Update wifi_aptest.py

--- wifi_scanner/wifi_aptest.py
+++ wifi_scanner/wifi_aptest.py
@@ -5,15 +5,16 @@ import wifi_scan_common_tools as wctools
 import json
 import requests, traceback
 
+
 #falta ssid
 reg_list = [
-    re.compile(r"^\d+\s+packets transmitted,\s+\d+\s+received,\s+(?P<loss>[\d+\.]+)\% packet loss,\s+time \d+ms"),
+    re.compile(r"^\d+\s+packets transmitted,\s+\d+\s+received,\s+(?P<loss>[\d\.]+)\% packet loss,\s+time \d+ms"),
     re.compile(r"^rtt min/avg/max/mdev = (?P<minv>[\d\.]+)/(?P<avgv>[\d\.]+)/(?P<maxv>[\d\.]+)")
 ]
 
 macaddr = wctools.get_macaddr()
 
-def parse(content,reg_list,additional_info={}):
+def parse(content,reg_list):
     """parse the output of a cmd output in to a dictionary of tokens
 """
     info = {}
@@ -26,7 +27,7 @@ def parse(content,reg_list,additional_info={}):
     return info
 
 url = "www.google.com"
-def scan(interface='wlan0'):
+def scan(url, interface='wlan0'):
     cmd = "ping -c {count} -i {interval} -I {interface} -W {timeout} -w {deadline} {url}"
     params = {
         "count":config.count,
@@ -36,14 +37,15 @@ def scan(interface='wlan0'):
         "deadline":config.deadline,
         "url":url}
     content = wctools.get_command_output(cmd.format(**params))
-    additional_info={
+    cells = parse(content,reg_list)
+    cells.update({
         "fecha":wctools.get_datetime_str(config.datetime_format_data),
         "macDispositivo":macaddr,
-    } #falta macssid, dbm, conexion exitosa, direccionweb
-    cells = parse(content,reg_list,additional_info=additional_info)
+        "url":url
+    }) #falta convertir perdidas a unidades, macssid, dbm, conexion exitosa
     return cells
-print(scan())
-            
+print(scan(url))
+
 def save_data(remain):
     file_name = wctools.get_datetime_str(config.datetime_format_file)+".json"
     with open(config.files_path+"/"+file_name,"w+") as f:
@@ -72,10 +74,43 @@ def init_f():
     return
 init_args = ()
 
+def update_from_db():
+    """read DB and returns a dict with the data
+
+    The returned dict must have the following structure:
+    {
+        ssid1:{
+            channel11:[
+                url11_1,
+                ...
+                url11_n
+                ]
+            ],
+            ...
+            channel1n:{...}
+        }
+        ...
+        ssidn:{...}
+    }
+    where ssidn is a string, channel1n is an int and
+    url11_n is a string.
+    """
+    return {
+        "JaverianaCali":{
+            1:["www.google.com"]
+        }
+    }
+
 def main_f(pending):
     print(pending)
     print("scanning")
-    jsonList = scan()
+    targets = update_from_db()
+    jsonList = []
+    for ssid in targets:
+        for channel in targets[ssid]:
+            #in this step wireless connection must be performed
+            for url in targets[ssid][channel]:
+                jsonList.append(scan(url))
     remain = send_data_to_server(jsonList)
     if remain:
         file_name = save_data(remain)
