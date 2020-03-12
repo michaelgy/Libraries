e4a9baabda25fb068eae435e5da449580a58f5c8
michaelgy <michael801898@gmail.com>
2020-03-12 12:57:22 -0500

    Add files via upload

---
+++ wifi_scanner/config_general.py
@@ -0,0 +1,4 @@
+sample_time = 10#10800 #seconds [3 hours]
+reconnect_time = 3#600 #seconds [10 minutes]
+sync_time = 3600 #tiempo entre sincronización con la base de datos
+debug = False
--- wifi_scanner/wifi_aptest.py
+++ wifi_scanner/wifi_aptest.py
@@ -1,21 +1,70 @@
-import re
-import wifi_aptest.config as config
+#!/usr/bin/python3
+#Run as sudo for complete AP list
+import wifi_aptest.config as config_aptest
+import wifi_scanner.config as config_scanner
+import config_general as config
 from os import listdir,remove
 import wifi_scan_common_tools as wctools
 import json
 import requests, traceback
+import subprocess
+from datetime import datetime
+import re
+import time
+from os import listdir
 
+
+regex_scan = [
+    re.compile(r"Address:\s(?P<ssidMac>.+)$"),
+    re.compile(r"^Frequency:(?P<bandaFrecuencia>[\d.]+)\s+GHz\s+\(Channel (?P<canal>\d+)\)$"),
+    re.compile(r"Signal level=(?P<dbm>.+) d.+$"),
+    re.compile(r"^ESSID:\"(?P<ssidNombre>.*)\"$")
+]
 
 #falta ssid
-reg_list = [
+regex_ping = [
     re.compile(r"^\d+\s+packets transmitted,\s+\d+\s+received,\s+(?P<loss>[\d\.]+)\% packet loss,\s+time \d+ms"),
     re.compile(r"^rtt min/avg/max/mdev = (?P<minv>[\d\.]+)/(?P<avgv>[\d\.]+)/(?P<maxv>[\d\.]+)")
 ]
 
 macaddr = wctools.get_macaddr()
 
-def parse(content,reg_list):
-    """parse the output of a cmd output in to a dictionary of tokens
+def get_command_output(cmd):
+    proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return proc.stdout.read().decode('utf-8')
+
+def get_datetime_str(format_str):
+    return datetime.now().strftime(format_str)
+
+def get_macaddr(interface="eth0"):
+    rasp_mac = re.compile(r"link/ether\s+(?P<rasp_mac>(([a-z0-9]){2}:)+([a-z0-9]){2})")
+    macaddr = None
+    cmd_out = get_command_output("ip link show {}".format(interface))
+    for line in cmd_out.split("\n"):
+        result = rasp_mac.search(line)
+        if result is not None:
+            macaddr=result.groupdict()["rasp_mac"]
+            break
+    return macaddr
+
+def parse_scan(content,reg_list,additional_info={}):
+    """parse the output of iwlist output in to a list of dictionary of tokens
+"""
+    cells = []
+    i = 0
+    for line in content.split('\n'):
+        sres  = reg_list[i].search(line.strip())
+        if sres is not None:
+            if i==0:
+                cells.append(sres.groupdict())
+                cells[-1].update(additional_info)
+            else:
+                cells[-1].update(sres.groupdict())
+            i = (i+1)%len(reg_list)
+    return cells
+
+def parse_ping(content,reg_list):
+    """parse the output of ping output in to a dictionary of tokens
 """
     info = {}
     i = 0
@@ -26,33 +75,41 @@ def parse(content,reg_list):
             info.update(sres.groupdict())
     return info
 
-url = "www.google.com"
-def scan(url, interface='wlan0'):
+def scan(interface='wlan0'):
+    content = wctools.get_command_output("iwlist {} scan".format(interface))
+    additional_info={
+        "fecha":wctools.get_datetime_str(config_scanner.datetime_format_data),
+        "macDispositivo":macaddr,
+        "ubicacion":config_scanner.ubicacion
+    }
+    cells = parse_scan(content,regex_scan,additional_info=additional_info)
+    return cells
+
+def ap_url_test(url,interface='wlan0'):
     cmd = "ping -c {count} -i {interval} -I {interface} -W {timeout} -w {deadline} {url}"
     params = {
-        "count":config.count,
-        "interval":config.interval,
+        "count":config_aptest.count,
+        "interval":config_aptest.interval,
         "interface":interface,
-        "timeout":config.timeout,
-        "deadline":config.deadline,
+        "timeout":config_aptest.timeout,
+        "deadline":config_aptest.deadline,
         "url":url}
     content = wctools.get_command_output(cmd.format(**params))
-    cells = parse(content,reg_list)
+    cells = parse_ping(content,regex_ping)
     cells.update({
-        "fecha":wctools.get_datetime_str(config.datetime_format_data),
+        "fecha":wctools.get_datetime_str(config_aptest.datetime_format_data),
         "macDispositivo":macaddr,
         "url":url
     }) #falta convertir perdidas a unidades, macssid, dbm, conexion exitosa
     return cells
-print(scan(url))
 
-def save_data(remain):
+def save_data(remain,config):
     file_name = wctools.get_datetime_str(config.datetime_format_file)+".json"
     with open(config.files_path+"/"+file_name,"w+") as f:
         f.write(json.dumps(remain))
     return file_name
 
-def send_data_to_server(listDict):
+def send_data_to_server(listDict,config):
     while listDict:
         jsdict = listDict.pop()
         try:
@@ -69,10 +126,6 @@ def send_data_to_server(listDict):
             break
         
     return listDict
-
-def init_f():
-    return
-init_args = ()
 
 def update_from_db():
     """read DB and returns a dict with the data
@@ -101,23 +154,26 @@ def update_from_db():
         }
     }
 
-def main_f(pending):
-    print(pending)
+def main_f(pending_scan,pending_aptest):
     print("scanning")
     targets = update_from_db()
-    jsonList = []
+    sc_cells = scan()
+    aptest_cells = []
     for ssid in targets:
         for channel in targets[ssid]:
             #in this step wireless connection must be performed
             for url in targets[ssid][channel]:
-                jsonList.append(scan(url))
-    remain = send_data_to_server(jsonList)
+                aptest_cells.append(scan(url))
+    remain = send_data_to_server(sc_cells,config_scanner)
     if remain:
-        file_name = save_data(remain)
-        pending.append(file_name)
-main_args = ()
+        file_name = save_data(remain,config_scanner)
+        pending_scan.append(file_name)
+    remain = send_data_to_server(aptest_cells, config_scanner)
+    if remain:
+        file_name = save_data(remain,config_aptest)
+        pending_aptest.append(file_name)
 
-def pend_f(pending):
+def pend_f(pending,config):
     remain = False
     print("sending pending data")
     print(pending)
@@ -125,16 +181,42 @@ def pend_f(pending):
         file_name = pending.pop()
         with open(config.files_path+"/"+file_name,"r") as f:
             jsonList = json.loads(f.read())
-            remain = send_data_to_server(jsonList)
+            remain = send_data_to_server(jsonList,config)
         if not remain:
             print(file_name+" was sended")
         else:
             print(file_name, " remain data was not sended")
-            file_name_remain = save_data(remain)
+            file_name_remain = save_data(remain,config)
             pending.append(file_name_remain)
             print(file_name_remain)
         
         remove(config.files_path+"/"+file_name)
-pend_args = ()
 
+def main():
+    #Get the data that is pending for send
+    pending_scan = [f for f in listdir(config_scanner.files_path) if f.split(".")[-1] == config_scanner.data_file_ext]
+    pending_aptest = [f for f in listdir(config_aptest.files_path) if f.split(".")[-1] == config_aptest.data_file_ext]
+    total_time = 0
+    while True:
+        if total_time>=config.sample_time:
+            total_time = 0
+            
+        if total_time == 0:
+            main_f(pending_scan,pending_aptest)
+                
+        if pending_scan:
+            pend_f(pending_scan, config_scanner)
+        if pending_aptest:
+            pend_f(pending_aptest, config_aptest)
+            
+        if pending_scan or pending_aptest:
+            if config.debug:
+                print("waiting to send pending data")
+            total_time += config.reconnect_time
+            time.sleep(config.reconnect_time)
+        else:
+            if config.debug:
+                print("no pending")
+            time.sleep(config.sample_time-total_time)
+            total_time += config.sample_time
 #wctools.main(init_f, init_args, main_f, main_args, pend_f, pend_args, config, debug = False)
