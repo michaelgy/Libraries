cbded74d755fa90a177111c09a56085e90e8f9de
michael801898@gmail.com <michael.guerrero1@abbott.com>
2021-05-20 14:46:41 -0500

    Added file search using pattern regex

---
+++ ProcessFiles/FileGather/FileSeeker.py
@@ -0,0 +1,34 @@
+from typing import List, Union
+from pathlib import Path
+import re
+
+def get_files(root_dir_path: str, file_pattern: str, max_depth_search : Union[int, None] = None, current_depth : int = 0, flags_regex=re.I) -> List[str]:
+    p = Path(root_dir_path)
+    pf = []
+    if max_depth_search == None:
+        cont = True
+    else:
+        cont = max_depth_search > current_depth
+    if cont:
+        for pi in p.iterdir():
+            if pi.is_file() and re.fullmatch(file_pattern, pi.name, flags_regex):
+                pf.append(pi)
+            elif pi.is_dir():
+                pf.extend(get_files(pi, file_pattern, max_depth_search, current_depth+1))
+    return pf
+
+if __name__ == "__main__":
+    root_dir_path = "Z:\\Informacion de estudiante en practica\\Archivos para indicadores de mantto\\MTTO CORRECTIVO - DMS\\2020\\SEMANAS"
+    
+    """La siguiente expresión hace match de los archivos que:
+    -no comienzan con "~" o con "Copia"
+    -tienen: "mtto", "correc", "sem" y dos digitos en ese orden
+    -terminan con ".xlsx" o  "macro.xlsm"
+    """
+    file_pattern = "(?!~|Copia).*mtto.*correc.*.*sem.*\d\d((.xlsx)|(.*macro.xlsm))"
+    
+    r = get_files(root_dir_path, file_pattern)
+    for p in r:
+        print(p.name,"\t",str(p))
+    print(len(r))
+    
--- ProcessFiles/Processing/ProcessWeeksExcel1.py
+++ ProcessFiles/Processing/ProcessWeeksExcel1.py
@@ -3,14 +3,17 @@ Funciona para las semanas 01 a 08 de los reportes del 2020
 """
 from BaseClasses import ProcessFileWF
 from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
 import re
+from openpyxl.styles.colors import Color
+from copy import copy
 
 class main(ProcessFileWF):
-    def __init__(self, input_file, output_file):
+    def __init__(self, input_file:str, output_file:str):
         super().__init__(input_file, output_file)
         self.process()
     
-    def process_sheet(self, input_sheet, output_sheet, output_startrow):
+    def process_sheet(self, input_sheet:"Worksheet object", output_sheet:"Worksheet object", output_startrow:int):
         """input_sheet: hoja de datos donde se van a extraer los datos
         output_sheet: hoja de datos donde se van a guardar los datos
         output_startrow: fila de output_sheet donde se comienzan a escribir los datos
@@ -28,7 +31,7 @@ class main(ProcessFileWF):
             rdi =rd+1
             columna_busqueda = 2
             columna_inicio_datos = 2 #Columna de input_sheet donde comienzan los datos
-            columna_final_datos = 7 #Columna de input_sheet donde terminan los datos
+            columna_final_datos = 8 #Columna de input_sheet donde terminan los datos
             columna_inicio = 2 #Columna de output_sheet donde comienzan los datos
             v = input_sheet.cell(row=rdi, column=columna_busqueda).value.strip().lower()
             if not re.fullmatch(".*sin.*novedad.*", v): #Verificar que hayan novedades para copiar
@@ -37,7 +40,15 @@ class main(ProcessFileWF):
                     rdf+=1
                 for r in range(rdi,rdf):
                     for c in range(columna_inicio_datos, columna_final_datos+1):
-                        output_sheet.cell(row=output_startrow+r-rdi, column=columna_inicio+c-columna_inicio_datos).value = input_sheet.cell(row=r, column=c).value
+                        cout = output_sheet.cell(row=output_startrow+r-rdi, column=columna_inicio+c-columna_inicio_datos)
+                        cin = input_sheet.cell(row=r, column=c)
+                        cout.value = cin.value
+                        if r == rdi+1:
+                            print("-----"*20)
+                            print(cin.fill.bgColor)
+                            print("-----"*20)
+                        cout.fill =  copy(cin.fill)
+
                     t = t +1
         print("\t {} {} {}".format("Se escribieron", t, "filas"))
     def process(self):
